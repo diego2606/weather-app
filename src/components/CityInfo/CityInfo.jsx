2aa0f535c79d3133b453d3867c4b8011db6938ba
diego2606 <diego.velasquez.ore@gmail.com>
2024-08-09 13:13:09 -0500

    feat: :sparkles: add Storybook stories for CityInfo component
    Added Storybook stories to demonstrate the different states and usage of the CityInfo component.

--- src/components/CityInfo/CityInfo.jsx
+++ src/components/CityInfo/CityInfo.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { typography } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
 
 const CityInfo = ({ city, country }) => {
   return (
-    <></>
+    <>
+        <Text>{city}</Text>
+        <Text>{country}</Text>
+    </>
   )
 }
 
